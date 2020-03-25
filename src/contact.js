d46dda604e79bd2da0e6f877cf4f8c3e69ded304
Christian Otieno <oduorchristian@gmail.com>
2020-03-25 16:02:40 +0300

    Add dom logic for home menu and contact page
    In this section, I added the rendering of the home view page to include a brief introduction on what the restraunt was all about. I then went ahead and added the menu section of the page and finished by adding the contact section. All are of which are being rendered by the javasript with respect to the manipulation of the dom

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,37 @@
-const contactPage = () => {
-  console.log('This is the contact landing page');
+const contantContent = document.querySelector('#Content');
+
+
+const contactHeader = (text) => {
+  const contact = document.createElement('h3');
+  contact.textContent = text;
+
+  return contact;
 };
 
+const contactDetails = (text) => {
+  const addressDetails = document.createElement('p');
+  addressDetails.innerHTML = text;
+
+  return addressDetails;
+};
+
+const contactNumber = (text) => {
+  const number = document.createElement('p');
+  number.textContent = text;
+
+  return number;
+};
+
+const contactPage = () => {
+  const contact = contactHeader('Contact Us');
+  const addressDetails = contactDetails('foothil 22786 - 3534 riverside drive.');
+  const number = contactNumber('+122345567');
+
+
+  contantContent.innerHTML = '';
+  contantContent.classList.remove('backddrop');
+  contantContent.appendChild(contact);
+  contantContent.appendChild(addressDetails);
+  contantContent.appendChild(number);
+};
 export default contactPage;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,38 @@ import homePage from './home';
 import menuPage from './menu';
 import contactPage from './contact';
 
-homePage();
-menuPage();
-contactPage();
+// header child-nodes
+const toggles = document.querySelectorAll('.nav-item');
+const homeToggle = document.querySelector('#home-toggle');
+const menuToggle = document.querySelector('#menu-toggle');
+const contactToggle = document.querySelector('#contact-toggle');
+const contentDiv = document.querySelector('#content');
 
-console.log('Hello, welcome to the restraunt page!');
+const selectToggle = (toggleButton) => {
+  toggles.forEach(toggle => {
+    if (toggle === toggleButton) {
+      toggle.classList.add('nav-item-selected');
+    } else {
+      toggle.classList.remove('nav-item-selected');
+    }
+  });
+};
+
+// event handlers
+homeToggle.addEventListener('click', function () {
+  selectToggle(this);
+  homePage();
+});
+menuToggle.addEventListener('click', function () {
+  selectToggle(this);
+  menuPage();
+});
+contactToggle.addEventListener('click', function () {
+  selectToggle(this);
+  contactPage();
+});
+contentDiv.addEventListener('animationend', function () {
+  this.classList.remove('down-enter-active');
+});
+
+homePage();
